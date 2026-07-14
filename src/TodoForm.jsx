832ff18ea8d3ecab37adca4305648a265adc069b
Yolanda Haynes <yhaynes@giyo.us>
2026-07-13 18:25:07 -0700

    added non-working todo form

---
+++ src/TodoForm.jsx
@@ -0,0 +1,12 @@
+function TodoForm() {
+    return (
+        <form>
+            <label htmlFor='todoTitle'>Todo</label>
+            <input type='text' id='todoTile' />
+            <button type='submit' disabled>Add Todo</button>
+        </form>
+
+    );
+}
+
+export default TodoForm;
