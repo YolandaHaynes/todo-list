a8c3cd6ce52dd3a776493fb41bd00edae819b7a9
Yolanda Haynes <yhaynes@giyo.us>
2026-08-03 18:49:23 -0700

    completed lesson 05

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,18 +1,22 @@
 import {useRef} from 'react';
+import { useState } from 'react'
 
 
 function TodoForm({onAddTodo}) {
+    const [workingTodoTitle, setWorkingTodoTitle] = useState("")
+
     const inputRef=useRef();
 
     const handleAddTodo = (event) => {event.preventDefault();
 
-    const todoTitle = event.target.todoTitle.value.trim();
+    const todoTitle = workingTodoTitle.trim();
     if (todoTitle && todoTitle !== "") {
       onAddTodo(todoTitle);
-      event.target.reset();
+      setWorkingTodoTitle("");
       inputRef.current.focus();
 
     }
+
 };
     return (
         <form onSubmit={handleAddTodo}>
@@ -24,8 +28,10 @@ function TodoForm({onAddTodo}) {
             name="todoTitle"
             placeholder={'Todo text'}
             required
+            value={workingTodoTitle}
+            onChange={(event)=> setWorkingTodoTitle(event.target.value)}
             />
-            <button type="submit">
+            <button type="submit" disabled={!workingTodoTitle.trim()}>
             Add Todo
             </button>
         </form>
